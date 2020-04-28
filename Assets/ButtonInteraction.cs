b8ffba6df164a593b30643ab13fb7b2add5346e6
Hristijan Anastasovski <anastasovski.h@gmail.com>
2020-04-28 22:15:46 +0200

    VR Office 2.0

---
+++ Assets/ButtonInteraction.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class ButtonInteraction : MonoBehaviour
+{
+    public float gazeTime = 2f;
+    private float timer;
+    private bool gazedAt;
+    public GameObject player;
+    public GameObject descriptionText;
+    private bool showText = false;
+    public TextMeshProUGUI descriptionButtonTitle;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gazedAt)
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= gazeTime)
+            {
+                // execute pointerdown handler
+                ExecuteEvents.Execute(gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerDownHandler);
+                timer = 0f;
+            }
+        }
+
+    }
+
+    public void newGame()
+    {
+        player.transform.position = new Vector3((float)9.45, (float)1.82, (float)-9.02);
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+        
+    }
+
+    public void showHideDescription()
+    {
+        if (!showText)
+        {
+            descriptionButtonTitle.text = "Hide Info";
+            descriptionText.SetActive(true);
+            showText = true;
+        } else
+        {
+            descriptionButtonTitle.text = "Show Info";
+            descriptionText.SetActive(false);
+            showText = false;
+        }
+    }
+
+    public void PointerEnter()
+    {
+        gazedAt = true;
+        Debug.Log("PointerEnter");
+    }
+
+    public void PointerExit()
+    {
+        gazedAt = false;
+        Debug.Log("PointerExit");
+    }
+
+    public void PointerDown()
+    {
+        Debug.Log("PointerDown");
+    }
+}
---
+++ Assets/VRFPSControll.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRFPSControll : MonoBehaviour
+{
+    public Transform vrCamera;
+    public float toggleAngle = 30.0f;
+    public float speed = 3.0f;
+    public bool moveForward;
+    public float jumpHeight = 500f;
+
+    private CharacterController cc;
+    private AudioSource aSrc;
+    private GameObject player;
+
+    public Vector3 startPosition;
+    // Use this for initialization
+
+    private void Awake()
+    {
+        startPosition = transform.position;
+    }
+
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+        aSrc = GetComponent<AudioSource>();
+        player = GameObject.Find("Player");
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (vrCamera.eulerAngles.x >= toggleAngle && vrCamera.eulerAngles.x < 90.0f)
+        {
+            moveForward = true;
+        }
+        else
+        {
+            moveForward = false;
+        }
+
+        if (moveForward)
+        {
+            if (!aSrc.isPlaying)
+            {
+                aSrc.Play();
+            }
+            Vector3 forward = vrCamera.TransformDirection(Vector3.forward);
+
+            cc.SimpleMove(forward * speed);
+        } else
+        {
+            if (aSrc.isPlaying)
+            {
+                aSrc.Stop();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // Android close icon or back button tapped.
+            Application.Quit();
+        }
+
+    }
+
+}
